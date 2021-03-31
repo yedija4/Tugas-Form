2341104da13f357a69be10d64c7e2fa2142c7e1b
eldwinp <80031253+eldwinp@users.noreply.github.com>
2021-03-31 16:56:27 +0700

    Tambahin fitur validation
    Tambahin validation gan

--- form.php
+++ form.php
@@ -5,37 +5,49 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container">
-<h2>Form Pendaftaran Mahasiswa</h2>
-    <form action="simpan-pendaftaran.php" method="post">
+ 
+<div class="container p-3 my-3 border">
+<h1>Form Pendaftaran Mahasiswa</h1>
+
+    <form action="simpan-pendaftaran.php" class="was-validated" method="post">
         <div class="form-group">
             <label>Username:</label>
-            <input type="text" name="username" class="form-control" placeholder="Masukan Username" />
+            <input type="text" name="username" class="form-control" placeholder="Masukan Username" required/>
+            <div class="valid-feedback">Valid.</div>
+    <div class="invalid-feedback">Please fill out this field.</div>
         </div>
-	<div class="form-group">
+  <div class="form-group">
             <label>Nama:</label>
-            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" />
+            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
+             
         </div>
-	<div class="form-group">
-	     <label>Alamat:</label>
-	     <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" ></textarea>
-	</div> 
-	<div class="form-group">
+
+  <div class="form-group">
+       <label>Alamat:</label>
+       <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
+  </div> 
+  <div class="form-group">
             <label>Email:</label>
-            <input type="email" name="email" class="form-control" placeholder="Masukan Email" />
+            <input type="email" name="email" class="form-control" placeholder="Masukan Email" required="" />
+            <div class="valid-feedback">Valid.</div>
+    <div class="invalid-feedback">Email not valid</div>
         </div>
-	<div class="form-group">
+  <div class="form-group">
             <label>No HP:</label>
-            <input type="number" name="no_hp" class="form-control" placeholder="Masukan No HP" />
+            <input type="number" name="no_hp" class="form-control" placeholder="Masukan No HP" required="" />
         </div>
-	<div class="form-group">
+  <div class="form-group">
             <label>Password:</label>
-            <input type="password" name="password" class="form-control" placeholder="Masukan Password" />
+            <input type="password" name="password" class="form-control" placeholder="Masukan Password" required="" />
+            <div class="valid-feedback">Valid.</div>
+    <div class="invalid-feedback">Please fill out this field.</div>
         </div>
 
         <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 
     </form>
+
+
 </div>
 </body>
 </html>
