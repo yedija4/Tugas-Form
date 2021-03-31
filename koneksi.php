c2a4c5f619f6ad08b173d462c15c28641fd3f195
ericz003 <76954409+ericz003@users.noreply.github.com>
2021-03-31 22:54:41 +0700

    Menambah koneksi
    Buat koneksi

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+    $host="localhost";
+    $user="root";
+    $password="";
+    $db="mahasiswa";
+
+    $kon= mysqli_connect($host,$user,$password,$db);
+
+    if (!$kon){
+        die("Koneksi gagal:".mysqli_connect_error());
+    }else{
+        echo "koneksi berhasil";
+    }
+?>
