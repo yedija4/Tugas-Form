2eefd93143b2b46d7cfe3ab5d5bcd5df2368fd97
eldwinp <80031253+eldwinp@users.noreply.github.com>
2021-03-31 15:13:45 +0700

    Update form.php
    Masukkan Bootstrap gan

--- form.php
+++ form.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Form Pendaftaran Mahasiswa</title>
+    <!-- Load file CSS Bootstrap offline -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-
+<div class="container">
 <h2>Form Pendaftaran Mahasiswa</h2>
     <form action="simpan-pendaftaran.php" method="post">
-	<table>
-          <tr>
-              <td><label>Username:</label></td><td><input type="text" name="username" placeholder="Masukan Username" /></td>
-	  </tr>
-	  <tr>
-	      <td><label>Nama:</label></td><td><input type="text" name="nama" placeholder="Masukan Nama" /></td>
-	  </tr>
-	  <tr>
-	      <td><label>Alamat:</label></td><td><textarea name="alamat" rows="5" placeholder="Masukan Alamat" ></textarea></td>
-	  </tr>
-	  <tr>
-              <td><label>Email:</label></td><td><input type="email" name="email" placeholder="Masukan Email" /></td>
-	  </tr>
-	  <tr>
-	      <td><label>No HP:</label></td><td><input type="text" name="no_hp" placeholder="Masukan No HP" /></td>
-	  </tr>
-	  <tr>
-	      <td><label>Password:</label></td><td><input type="password" name="password" placeholder="Masukan Password" /></td>
-	   </tr>
-	</table>
-      <button type="submit" name="submit">Submit</button>
+        <div class="form-group">
+            <label>Username:</label>
+            <input type="text" name="username" class="form-control" placeholder="Masukan Username" />
+        </div>
+	<div class="form-group">
+            <label>Nama:</label>
+            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" />
+        </div>
+	<div class="form-group">
+	     <label>Alamat:</label>
+	     <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" ></textarea>
+	</div> 
+	<div class="form-group">
+            <label>Email:</label>
+            <input type="email" name="email" class="form-control" placeholder="Masukan Email" />
+        </div>
+	<div class="form-group">
+            <label>No HP:</label>
+            <input type="number" name="no_hp" class="form-control" placeholder="Masukan No HP" />
+        </div>
+	<div class="form-group">
+            <label>Password:</label>
+            <input type="password" name="password" class="form-control" placeholder="Masukan Password" />
+        </div>
+
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+
     </form>
+</div>
 </body>
 </html>
